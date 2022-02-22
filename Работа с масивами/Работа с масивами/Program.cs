9bee5b51572910b9a15e2e536a65606420e97e9f
dmitry126 <100125125+dmitry126@users.noreply.github.com>
2022-02-22 12:04:22 +0300

    Добавлены коменты

--- Работа с масивами/Работа с масивами/Program.cs
+++ Работа с масивами/Работа с масивами/Program.cs
@@ -5,24 +5,23 @@ namespace Работа_с_масивами
     {
         static void Main(string[] args)
         {
-            int counter = 0;
-            int sum = 0;
-            int[] A = new int[9];
-            Console.WriteLine("Привет, прога считает среднее арифметическое между 9 введенными числами которые больше 12 и меньше 25 ");
-            for (int i = 0; i <= 8; i++)
-            {
-                Console.WriteLine($"Введите число #{i+1}:") ;
-                int y = Convert.ToInt32(Console.ReadLine());
-                A[i] = y;
+            int counter = 0; //счетчик
+            int sum = 0;    //сумма
+            int[] A = new int[9];//создаем массив
+            Console.WriteLine("Привет, прога считает среднее арифметическое между 9 введенными числами которые больше 12 и меньше 25 ");//приветствие
+            for (int i = 0; i <= 8; i++){ //создаем цикл для ввода массива
+                Console.WriteLine($"Введите число #{i+1}:");//запрос на ввод числа
+                int y = Convert.ToInt32(Console.ReadLine());//ввод числа
+                A[i] = y;//присваиваем элементу массива введенное число
             }
-            foreach (int element in A) {
-                if (element > 12 && element < 25) {
-                    sum += element;
-                    counter++;
+            foreach (int element in A) {//цикл по всем элементам массива
+                if (element > 12 && element < 25){//условие
+                    sum += element;//суммирование
+                    counter++;//счетчик
                 }
             }
-            Console.WriteLine($"Cреднее арифметическое между 9 введенными числами которые больше 12 и меньше 25 = {(double)sum/counter}");
-            Console.ReadLine();
+            Console.WriteLine($"Cреднее арифметическое между 9 введенными числами которые больше 12 и меньше 25 = {(double)sum/counter}");//вывод результата с приведением к типу double
+            Console.ReadLine();//ожидание нажатия клавиши
         }
     }
 }
